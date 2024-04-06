80aae95f385b611e407c449f78d624c4016a8203
Victor Vinicius <victormafiavinicius@gmail.com>
2024-04-06 09:30:39 -0300

    Implementing simple search, using asp net core 8.0

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartamentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
