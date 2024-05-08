7a466e89011ba7f4d9a5220bb9b715862a38809d
Victor Vinicius <victormafiavinicius@gmail.com>
2024-05-08 11:29:32 -0300

    Complete WebAPI update. Here, I added an area with Notes, to save messages from my project. Updates were also made to the controllers and the entire layout.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
+using System.Diagnostics;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App From C# Course";
+            ViewData["email"] = "Victor Vinicius";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new SalesWebMvc.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public string Message { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
